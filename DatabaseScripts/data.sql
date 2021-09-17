ae5371cb9ad6bbbf936998beed011a013757bcec
Admin <>
2021-09-17 15:15:33 +1200

    Add project files.

---
+++ DatabaseScripts/data.sql
@@ -0,0 +1,6 @@
+SET IDENTITY_INSERT [dbo].[PetCareSession] ON
+INSERT INTO [dbo].[PetCareSession] ([Id], [PetName], [PetType], [Start], [End]) VALUES (5, N'Molly', N'Dog', N'2019-11-28 09:00:00', N'2019-11-28 11:00:00')
+INSERT INTO [dbo].[PetCareSession] ([Id], [PetName], [PetType], [Start], [End]) VALUES (6, N'Charlie', N'Dog', N'2019-11-30 10:25:00', N'2019-11-30 12:25:00')
+INSERT INTO [dbo].[PetCareSession] ([Id], [PetName], [PetType], [Start], [End]) VALUES (7, N'Tilly', N'Cat', N'2019-11-30 12:25:00', N'2019-11-30 14:25:00')
+INSERT INTO [dbo].[PetCareSession] ([Id], [PetName], [PetType], [Start], [End]) VALUES (8, N'Luna', N'Cat', N'2019-12-01 12:25:00', N'2019-12-01 14:25:00')
+SET IDENTITY_INSERT [dbo].[PetCareSession] OFF
